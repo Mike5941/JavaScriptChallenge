803806a88ff59a36d649a5206ba05bc0de528e6c
mike5941 <mike9159@naver.com>
2024-05-09 09:16:07 +0900

    array-method 학습

--- 8.array/3.array-method.js
+++ 8.array/3.array-method.js
@@ -44,4 +44,35 @@ console.log(newArr);
 console.log(fruits);
 
 // 여러 개의 배열을 합침
-const arr1 = [1,2,3]
+const arr1 = [1, 2, 3];
+const arr2 = [3, 4, 5];
+const arr3 = arr1.concat(arr2);
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+// 배열 순서를 거꾸로
+const arr4 = arr3.reverse();
+console.log(arr4);
+
+// 중첩된 배열을 펼치기 (단계 입력)
+let arr = [
+  [1,2,3], [4, [5,6]],
+];
+arr = arr.flat(2)
+
+// 특정한 값으로 배열을 채우기
+arr.fill('s', 1, 3);
+console.log(arr);
+arr.fill(0)
+console.log(arr);
+
+// 배열을 문자열로 합하기
+let text = arr.join();
+console.log(text);
+text = arr.join(' | ')
+console.log(text);
+
+// 배열 자체를 수정하는지, 반환된 값으로 새로운 배열을 반환하는지가 포인트
+// parameter를 전달하는 경우 어떤 조합으로 사용할 수 있는지
